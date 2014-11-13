7e13c60ff58b6dc926fa2479aa4e6ae17212f779
Trong Truong <nahn.truong@gmail.com>
2014-11-12 21:49:31 -0500

    following tutorial by kim pedersen

---
+++ Map.h
@@ -0,0 +1,21 @@
+//
+//  Map.h
+//  ProceduralLevelGeneration
+//
+//  Created by Michael T on 2014-11-11.
+//  Copyright (c) 2014 Kim Pedersen. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface Map : SKNode
+
+@property (nonatomic) CGSize gridSize;
+@property (nonatomic, readonly) CGPoint spawnPoint;
+@property (nonatomic, readonly) CGPoint exitPoint;
+@property (nonatomic) NSUInteger maxFloorCount;
+
++ (instancetype) mapWithGridSize:(CGSize)gridSize;
+- (instancetype) initWithGridSize:(CGSize)gridSize;
+
+@end
---
+++ Map.m
@@ -0,0 +1,42 @@
+//
+//  Map.m
+//  ProceduralLevelGeneration
+//
+//  Created by Michael T on 2014-11-11.
+//  Copyright (c) 2014 Kim Pedersen. All rights reserved.
+//
+
+#import "Map.h"
+
+@implementation Map
+
++ (instancetype) mapWithGridSize:(CGSize)gridSize
+{
+    return [[self alloc] initWithGridSize:gridSize];
+}
+
+- (instancetype) initWithGridSize:(CGSize)gridSize
+{
+    if (( self = [super init] ))
+    {
+        self.gridSize = gridSize;
+        _spawnPoint = CGPointZero;
+        _exitPoint = CGPointZero;
+        [self generateTileGrid];
+    }
+    return self;
+}
+
+- (void) generateTileGrid
+{
+    CGPoint startPoint = CGPointMake(self.gridSize.width / 2, self.gridSize.height / 2);
+    
+    NSUInteger currentFloorCount = 0;
+    
+    while ( currentFloorCount < self.maxFloorCount )
+    {
+        currentFloorCount++;
+    }
+}
+
+@end
